5fe6925ef28f8d8a1feb0b5f95f41d461ae7f5c0
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-07 19:52:22 -0400

    some renames

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>About me</h1>
+    </div>
+  );
 };
--- src/Components/pages/News.js
+++ src/Components/pages/News.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const News = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>News</h1>
+    </div>
+  );
 };
