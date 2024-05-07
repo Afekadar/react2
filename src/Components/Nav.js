43839c8c0dfe8cc413239dcecdb0de546b2225ba
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-07 12:10:38 -0400

    dropdown css and some comments

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { Link, NavLink } from "react-router-dom";
 
 export const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
-      <Link to="/Website" className='title'>Website</Link>
-      <ul>
+      <Link to="/Website" className="title">
+        Website
+      </Link>
+      <div
+        className="menu"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
         <li>
           <NavLink to="/Home">Home</NavLink>
         </li>
--- src/Components/pages/Website.js
+++ src/Components/pages/Website.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const Website = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      This is the website and here I will have content
+    </div>
+  );
 };
