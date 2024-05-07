628e8b9420dbf4772ae10772b60122d563fdb957
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-07 12:31:26 -0400

    js comments

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -13,14 +13,14 @@ export const Nav = () => {
         className="menu"
         onClick={() => {
           setMenuOpen(!menuOpen);
-        }}
+        }} 
       >
         <span></span>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul className={menuOpen ? "open" : ""}>
+      <ul className={menuOpen ? "open" : ""}> //if menuOpen exist then open else nothing
         <li>
           <NavLink to="/Home">Home</NavLink>
         </li>
