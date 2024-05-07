ec99e22bd3c57fdc0469ccfcaaa16b18134752f0
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-07 17:19:02 -0400

    logic start

--- src/Components/pages/Docs.js
+++ src/Components/pages/Docs.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Board } from "./TicTacToe/Board";
 
 export const Docs = () => {
-  const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return "X";
+      } else {
+        return value;
+      }
+    });
+
+    setBoard(updatedBoard);
+  };
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   );
 };
