314f443f338169a647ada4d0dad21a22461c1ed1
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-08 15:31:02 -0400

    scoreboard css

--- src/Components/pages/TicTacToe/ScoreBoard.js
+++ src/Components/pages/TicTacToe/ScoreBoard.js
@@ -5,8 +5,12 @@ export const ScoreBoard = ({ scores, xPlaying }) => {
   const { xScore, oScore } = scores;
   return (
     <div className="scoreboard">
-      <span className={"score x-score ${!xPlaying}"}>X-{xScore}</span>
-      <span className={"score o-score ${xPlaying}"}>O-{oScore}</span>
+      <span className={'score x-score ${!xPlaying && "inactive"}'}>
+        X-{xScore}
+      </span>
+      <span className={'score o-score ${xPlaying && "inactive"}'}>
+        O-{oScore}
+      </span>
     </div>
   );
 };
