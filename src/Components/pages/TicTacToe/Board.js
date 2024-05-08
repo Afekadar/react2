55e7a4c5db633d043b91c8ab2ca92817b99006eb
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-07 20:49:05 -0400

    comment fix

--- src/Components/pages/TicTacToe/Board.js
+++ src/Components/pages/TicTacToe/Board.js
@@ -3,8 +3,8 @@ import { Box } from "./Box";
 import "./Board.css";
 
 export const Board = ({ board, onClick }) => {
-  return (
-    <div className="board">//just a styling for the board to make it a 3x3 
+  return (//just a styling for the board to make it a 3x3 
+    <div className="board">
       {board.map((value, idx) => {
         return <Box value={value} onClick={() => onClick(idx)} />;
       })}
