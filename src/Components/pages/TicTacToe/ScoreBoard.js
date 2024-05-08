7816744f889e56029d50579b56c9e95dd984ce47
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-08 15:33:18 -0400

    bug fix yay thx chatgpt

--- src/Components/pages/TicTacToe/ScoreBoard.js
+++ src/Components/pages/TicTacToe/ScoreBoard.js
@@ -5,10 +5,10 @@ export const ScoreBoard = ({ scores, xPlaying }) => {
   const { xScore, oScore } = scores;
   return (
     <div className="scoreboard">
-      <span className={'score x-score ${!xPlaying && "inactive"}'}>
+      <span className={`score x-score ${!xPlaying && "inactive"}`}>
         X-{xScore}
       </span>
-      <span className={'score o-score ${xPlaying && "inactive"}'}>
+      <span className={`score o-score ${xPlaying && "inactive"}`}>
         O-{oScore}
       </span>
     </div>
